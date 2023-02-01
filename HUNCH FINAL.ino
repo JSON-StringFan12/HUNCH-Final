b73e0fe85d1783213cf92e74de13ef2bca5dbad1
JSON-StringFan12 <87513417+JSON-StringFan12@users.noreply.github.com>
2023-02-01 17:23:39 -0600

    Update HUNCH FINAL.ino

--- HUNCH FINAL.ino
+++ HUNCH FINAL.ino
@@ -4,32 +4,56 @@ Servo Scan;
 Servo MoveX;
 Servo MoveY;
 
+// Pin Configurations
+const int Pin_X = 2
+const int Pin_Y = 3
+const int Pin_Scan = 4
+
+// Variables to modify:
+// Servo Center Positions:
+// Center position of Servo
+const int Scan_Center = 94;
+const int X_Center = 70;
+const int Y_Center = 61;
+
+// Time between Measurements (ms)
+const int Measurement_Time = 6000;
+
+// Y Max Position (To prevent hitting the floor)
+const int Y_Max = 120;
+
+// Panel Measurements Per Degree (higher = more accurate but slower measurements)
+const int Measure_Per_Degree = 1000;
+
+// Range of Scan and Main Panels
+const int Range = 90;
+
+
 float Max[2][2] = {{0, 1}, {0, 1}};
 
-int lastX = 70;
-int lastY = 65;
+int lastX = X_Center;
+int lastY = Y_Center;
 
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(9600);
-  MoveX.attach(2);
-  MoveY.attach(3);
-  Scan.attach(4);
-  Scan.write(94); // 94 = center (+4)
-  MoveX.write(70); // 70 = center (+25)
-  delay(100);
-  MoveY.write(61); // 61 = center (+20)
+  Serial.begin(9601);
+  MoveX.attach(Pin_X);
+  MoveY.attach(Pin_Y);
+  Scan.attach(Pin_Scan);
+  Scan.write(Scan_Center);
+  MoveX.write(X_Center);
+  MoveY.write(Y_Center);
 }
 
 int readScan (int pin, int pos, int max, bool scan, int delay) {
   float avg = 0;
-  for (int i = 0; i < 1000; i++) {
+  for (int i = 0; i < Measure_Per_Degree; i++) {
     float read = analogRead(pin);
     read = read * 5/1023;
     avg += read;
     delayMicroseconds(delay);
   }
-  avg /= 1000;
+  avg /= Measure_Per_Degree;
   if (avg > Max[max][0] && scan == true) {
     Max[max][0] = avg;
     Max[max][1] = pos;
@@ -40,7 +64,6 @@ int readScan (int pin, int pos, int max, bool scan, int delay) {
 }
 
 int Move (int XPos, int YPos) {
-  Serial.println(XPos, YPos);
   if (lastX > XPos) {
     for (int i = lastX; i >= XPos; i--) {
       MoveX.write(i);
@@ -70,31 +93,24 @@ int Move (int XPos, int YPos) {
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  delay(6000);
-  for (int i = 49; i < 139; i++) {
+  delay(Measurement_Time);
+  for (int i = Scan_Center - (Range/2); i < Scan_Center + (Range/2); i++) {
     Scan.write(i);
-    readScan(A0, i, 0, true, 1);
+    readScan(A4, i, 0, true, 1);
     readScan(A1, i, 1, true, 1);
   }
-  if (Max[1][1] > 120) {
-    Max[1][1] = 120;
+  if (Max[1][1] > Y_Max) {
+    Max[1][1] = Y_Max;
   }
   Scan.write(Max[0][1]);
-  delay(1000);
+  delay(2000);
   Scan.write(Max[1][1]);
-  delay(1000);
-  Move((Max[1][1] - 94) + 70, (Max[0][1] - 94)/4 + 61);
-  MoveY.write((Max[0][1] - 94)/4 + 61);
-  MoveX.write((Max[1][1] - 94)/3 + 70);
-  delay(2-000);
-  Serial.println("Tracking:");
-  readScan(A2, 0, 0, false, 10000);
-  Serial.println("Static:");
-  readScan(A3, 0, 0, false, 10000);
+  delay(2000);
+  Move((Max[0][1] - Scan_Center) + X_Center, (Max[1][1] - Scan_Center) + Y_Center);
+  MoveY.write((Max[1][1] - Scan_Center) + Y_Center);
+  MoveX.write((Max[0][1] - Scan_Center) + X_Center);
   Max[0][0] = 0;
   Max[0][1] = 1;
   Max[1][0] = 0;
   Max[1][1] = 1;
-  delay(2000);
 }
